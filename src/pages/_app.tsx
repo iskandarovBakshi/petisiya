1228f54b4239b49a1b91fc10d1d47586c145184b
Baba Iskandarov <bifo92@gmail.com>
2023-05-02 16:32:14 +0400

    terms

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,10 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, ...appProps }: AppProps) {
+    if (["/terms-and-conditions"].includes(appProps.router.pathname)) {
+        return <Component {...pageProps} />;
+    }
     return (
         <Layout>
             <Component {...pageProps} />
